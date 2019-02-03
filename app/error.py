c432466345215baa37ff207e9616a2d1191d7007
nimomaina <nimomaina.nm@gmail.com>
2019-02-04 01:58:51 +0300

    Add articles class

--- app/error.py
+++ app/error.py
@@ -2,8 +2,8 @@ from flask import render_template
 from app import app
 
 @app.errorhandler(404)
-def four_Ow_four(error):
+def fourOfour(error):
     '''
     Function to render the 404 error page
     '''
-    return render_template('fourOwfour.html'),404
+    return render_template('404.html'),404
