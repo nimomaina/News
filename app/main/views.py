8c2694818232605ca3f1a68e44a559d373d94d3d
nimomaina <nimomaina.nm@gmail.com>
2019-02-04 02:53:51 +0300

    Correct errors in config file

--- app/main/views.py
+++ app/main/views.py
@@ -9,7 +9,7 @@ def index():
     '''
     view root page that returns the index page and its data
     '''
-    title = "Home || News Sources"
+    title = "Home - The World On Your Lap"
 
     all_news = get_news('sports')
     general_news = get_news('general')
--- config.py
+++ config.py
@@ -31,3 +31,4 @@ class DevConfig(Config):
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
+}
