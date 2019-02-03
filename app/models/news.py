6be67d740dd975679e0d52f6361c7d39ff4125ea
nimomaina <nimomaina.nm@gmail.com>
2019-02-04 00:36:54 +0300

    Create class news

---
+++ app/models/news.py
@@ -0,0 +1,13 @@
+class News:
+    '''
+    all news class to define news objects
+    '''
+
+    def __init__(self, name, author, url, description, country, category, id):
+        self.name = name
+        self.author = author
+        self.url = url
+        self.description = description
+        self.country = country
+        self.category = category
+        self.id = id
