6a604d46243eaaf99a5cdb5113b5fa8db9141cf8
nimomaina <nimomaina.nm@gmail.com>
2019-02-05 01:43:43 +0300

    Polished the application

--- app/main/views.py
+++ app/main/views.py
@@ -10,17 +10,16 @@ def index():
     view root page that returns the index page and its data
     '''
     title = "Home - The World On Your Lap"
-
     all_news = get_news('sports')
     general_news = get_news('general')
     tech_news = get_news('technology')
     bus_news = get_news('business')
-    sci_news = get_news('science')
+    ent_news = get_news('entertainment')
 
     # print(all_news)
 
     return render_template('index.html', title=title, sports=all_news, general=general_news, technology=tech_news,
-                           business=bus_news, science=sci_news)
+                           business=bus_news, entertainment=ent_news)
 
 
 # Views
